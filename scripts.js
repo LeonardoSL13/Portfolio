55114eb667c2af17a9a507232ef415842aeffd77
Leonardo Dos Santos Lopes <leos_lopes@hotmail.com>
2022-02-23 19:47:20 -0300

    add nav button on formacao

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let index = 2;
+let lastItem = 2;
+function nextSlider(){
+  if(index===lastItem){
+    index = 1;
+  }else{
+    index++ ;
+  }
+  document.getElementById("trigger"+index).checked = true; 
+  
+}
+function previousSlider(){
+  if(index===1){
+    index = lastItem;
+  }else{
+    index-- ;
+  }
+  document.getElementById("trigger"+index).checked = true; 
+ 
+}
