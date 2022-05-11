e9f24570f33c244fd7998a57907c74ceee95a572
Leonardo Dos Santos Lopes <leos_lopes@hotmail.com>
2022-05-11 00:58:06 -0300

    change index value and prettier format

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
-let index = 2;
+let index = 1;
 let lastItem = 2;
-function nextSlider(){
-  if(index===lastItem){
+function nextSlider() {
+  if (index === lastItem) {
     index = 1;
-  }else{
-    index++ ;
+  } else {
+    index++;
   }
-  document.getElementById("trigger"+index).checked = true; 
-  
+  document.getElementById("trigger" + index).checked = true;
+
 }
-function previousSlider(){
-  if(index===1){
+function previousSlider() {
+  if (index === 1) {
     index = lastItem;
-  }else{
-    index-- ;
+  } else {
+    index--;
   }
-  document.getElementById("trigger"+index).checked = true; 
- 
+  document.getElementById("trigger" + index).checked = true;
+
 }
